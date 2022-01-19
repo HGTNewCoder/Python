713544bcef81a0753f12787d7b6634a30c7d12e8
HGTNewCoder <thinh.hagia22@gmail.com>
2022-01-19 08:38:11 +0700

    Begin

---
+++ SmallProject/Pygame/FlappyBird/Flappy.py
@@ -0,0 +1,20 @@
+import pygame
+SCREENWIDTH = 600
+SCREENLENGTH = 500
+GRAY = (150,150,150)
+WHITE = (255,255,255)
+RED = (235, 52, 52)
+BLACK = (0,0,0)
+running = True
+
+pygame.init()
+pygame.display.set_caption("Flappy Bird")
+screen = pygame.display.set_mode((SCREENLENGTH, SCREENWIDTH))
+
+while running:
+    screen.fill(WHITE)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    pygame.display.flip()
+pygame.quit()
