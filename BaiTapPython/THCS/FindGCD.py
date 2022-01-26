2c295411d452c353635cd88d1b10bf703b4d4e69
HGTNewCoder <thinh.hagia22@gmail.com>
2022-01-26 13:22:29 +0700

    Update

---
+++ BaiTapPython/THCS/FindGCD.py
@@ -0,0 +1,25 @@
+from asyncio.windows_events import NULL
+import re
+
+
+a = int(input("Enter a: "))
+b = int(input("Enter b: "))
+
+def gcd1(a, b):
+    if a == 0 or b == 0:
+        return a + b
+    while a != b:
+        if a > b:
+            a -= b
+        else:
+            b -= a
+    print(a)
+
+def gcd2(a,b):
+    if b == 0:
+        gcd = a
+        print(gcd)
+        return gcd
+    return gcd2(b, a % b)
+
+gcd2(a, b)
